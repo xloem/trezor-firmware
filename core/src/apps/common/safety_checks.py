8fd822d0d845ee8e662ec6a62d57ec8b4592abb7
matejcik <ja@matejcik.cz>
2021-02-25 15:37:02 +0100

    refactor(core): modify cache to preallocate all its data
    also get rid of expensive "wire" import

--- core/src/apps/common/safety_checks.py
+++ core/src/apps/common/safety_checks.py
@@ -5,7 +5,6 @@ from storage.device import SAFETY_CHECK_LEVEL_PROMPT, SAFETY_CHECK_LEVEL_STRICT
 from trezor.messages import SafetyCheckLevel
 
 if False:
-    from typing import Optional
     from trezor.messages.ApplySettings import EnumTypeSafetyCheckLevel
 
 
@@ -13,11 +12,9 @@ def read_setting() -> EnumTypeSafetyCheckLevel:
     """
     Returns the effective safety check level.
     """
-    temporary_safety_check_level: Optional[
-        EnumTypeSafetyCheckLevel
-    ] = storage.cache.get(APP_COMMON_SAFETY_CHECKS_TEMPORARY)
-    if temporary_safety_check_level is not None:
-        return temporary_safety_check_level
+    temporary_safety_check_level = storage.cache.get(APP_COMMON_SAFETY_CHECKS_TEMPORARY)
+    if temporary_safety_check_level:
+        return int.from_bytes(temporary_safety_check_level, "big")  # type: ignore
     else:
         stored = storage.device.safety_check_level()
         if stored == SAFETY_CHECK_LEVEL_STRICT:
@@ -33,14 +30,14 @@ def apply_setting(level: EnumTypeSafetyCheckLevel) -> None:
     Changes the safety level settings.
     """
     if level == SafetyCheckLevel.Strict:
-        storage.cache.delete(APP_COMMON_SAFETY_CHECKS_TEMPORARY)
+        storage.cache.set(APP_COMMON_SAFETY_CHECKS_TEMPORARY, b"")
         storage.device.set_safety_check_level(SAFETY_CHECK_LEVEL_STRICT)
     elif level == SafetyCheckLevel.PromptAlways:
-        storage.cache.delete(APP_COMMON_SAFETY_CHECKS_TEMPORARY)
+        storage.cache.set(APP_COMMON_SAFETY_CHECKS_TEMPORARY, b"")
         storage.device.set_safety_check_level(SAFETY_CHECK_LEVEL_PROMPT)
     elif level == SafetyCheckLevel.PromptTemporarily:
         storage.device.set_safety_check_level(SAFETY_CHECK_LEVEL_STRICT)
-        storage.cache.set(APP_COMMON_SAFETY_CHECKS_TEMPORARY, level)
+        storage.cache.set(APP_COMMON_SAFETY_CHECKS_TEMPORARY, level.to_bytes(1, "big"))
     else:
         raise ValueError("Unknown SafetyCheckLevel")
 
