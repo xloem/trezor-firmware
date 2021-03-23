efb0f506999dbefc86363d015bb2272e82074625
Jan Pochyla <jpochyla@gmail.com>
2021-03-23 13:37:37 +0100

    WIP: Rust UI

--- core/embed/rust/librust.h
+++ core/embed/rust/librust.h
@@ -1,5 +1,8 @@
 #include "librust_qstr.h"
 
+mp_obj_t ui_render_rich_text(const mp_map_t *kw);
+mp_obj_t ui_layout_new_example(void);
+
 mp_obj_t protobuf_type_for_name(mp_obj_t name);
 mp_obj_t protobuf_type_for_wire(mp_obj_t wire_id);
 mp_obj_t protobuf_decode(mp_obj_t buf, mp_obj_t def,
